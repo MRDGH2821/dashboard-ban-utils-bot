0223e32978af213a533360537c47964b4821adb4
MRDGH2821 <mihirrabade@gmail.com>
2022-01-14 16:29:05 +0530

    feat(homepage): add buttons in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,1 +1,28 @@
-export const HomePage = () => <div>HomePage</div>;
+import { FaDiscord, FaQuestionCircle } from 'react-icons/fa';
+import { DiscordLoginButton, SupportButton, HomePageStyle } from '../utils/styles/HomePage'
+
+export const HomePage = () => (
+  <HomePageStyle>
+    <div></div>
+    <div>
+      <DiscordLoginButton>
+        <FaDiscord size={45} />
+        <p style={{ fontSize: '20px' }}>Login with Discord</p>
+      </DiscordLoginButton>
+      <SupportButton>
+        <FaQuestionCircle size={45} />
+        <p style={{ fontSize: '20px' }}>Support server</p>
+      </SupportButton>
+    </div>
+    <div style={{
+      display: 'flex',
+      width: '450px',
+      justifyContent: 'space-between',
+
+    }}>
+      <span>Privacy Policy</span>
+      <span>Terms of Service</span>
+      <span>Contact Us</span>
+    </div>
+  </HomePageStyle>
+)
---
+++ src/utils/styles/HomePage.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const DiscordLoginButton = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 350px;
+background-color: #5865f2;
+padding: 4px 50px;
+box-sizing: border-box;
+border-radius: 5px;
+border: 1px transparent;
+margin: 4px 0;
+`;
+
+export const SupportButton = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 350px;
+background-color: #4f545c;
+padding: 4px 50px;
+box-sizing: border-box;
+border-radius: 5px;
+border: 1px;
+`;
+
+export const HomePageStyle = styled.div`
+height: 100%;
+padding: 100px 0;
+box-sizing: border-box;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+`;
