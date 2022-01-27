16d573a7fbcba0f63a87c4787c7dcedeaff89993
MRDGH2821 <mihirrabade@gmail.com>
2022-01-27 20:54:02 +0530

    feat(log setting): implement logs page

--- src/pages/LogsPage.tsx
+++ src/pages/LogsPage.tsx
@@ -1,1 +1,17 @@
-export const LogsPage = () => <div>LogsPage</div>;
+import { Title, Container, Page, Select } from '../utils/styles'
+export const LogsPage = () => <Page>
+  <Container>
+    <Title>Update Logs Channel </Title>
+    <div>
+      <div>
+        <label>Current Channel</label>
+      </div>
+      <Select>
+        <option disabled> Please Select a channel </option>
+        <option> 123 </option>
+        <option selected value="124"> #welcome (124)</option>
+        <option> 345</option>
+      </Select>
+    </div>
+  </Container>
+</Page>;
