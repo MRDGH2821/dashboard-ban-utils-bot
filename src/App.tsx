b48dfd6618f869fb51e0bbd898ff0692e42a2f43
MRDGH2821 <mihirrabade@gmail.com>
2022-02-06 13:13:40 +0530

    feat: add login guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
 import { MenuPage } from './pages/MenuPage';
 import { CategoryPage } from './pages/CategoryPage';
 import { LogsPage } from './pages/LogsPage';
-import { GuildContext } from './utils/contexts/GuildContext'
+import { GuildContext } from './utils/contexts/GuildContext';
 import { AppBar } from './components/AppBar';
+import { useFetchUser } from './utils/hooks/useFetchUser';
 
 function App() {
   const [guildId, setGuildId] = useState('');
+  const { user, error, loading } = useFetchUser();
   const updateGuildId = (id: string) => setGuildId(id);
   return (
     <GuildContext.Provider value={{ guildId, updateGuildId }}>
-      <Routes>
-        <Route path="/dashboard/*" element={<AppBar />} />
-      </Routes>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/menu' element={<MenuPage />} />
-        <Route path='/dashboard/categories' element={<CategoryPage />} />
-        {/*<Route path='/dashboard' element={<HomePage />} />*/}
-        <Route path='/dashboard/setlogs' element={<LogsPage />} />
-      </Routes>
+      {user ? (
+        <>
+          <Routes>
+            <Route path='/dashboard/*' element={<AppBar />} />
+          </Routes>
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='/menu' element={<MenuPage />} />
+            <Route path='/dashboard/categories' element={<CategoryPage />} />
+            {/*<Route path='/dashboard' element={<HomePage />} />*/}
+            <Route path='/dashboard/setlogs' element={<LogsPage />} />
+          </Routes>
+        </>
+      ) : (
+        <>
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+          </Routes>
+        </>
+      )}
     </GuildContext.Provider>
   );
 }
